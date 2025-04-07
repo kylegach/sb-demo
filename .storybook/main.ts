2949abad659bbbf2b2c637e9dedf5414bdd1340e
Kyle Gach <kyle.gach@gmail.com>
2025-04-07 11:36:57 -0600

    Add addon-a11y
    - Initialize VTA

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -6,10 +6,11 @@ const config: StorybookConfig = {
     "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"
   ],
   "addons": [
-    "@storybook/addon-essentials",
     "@storybook/addon-onboarding",
-    // "@chromatic-com/storybook",
-    "@storybook/experimental-addon-test"
+    "@storybook/addon-essentials",
+    "@storybook/experimental-addon-test",
+    "@storybook/addon-a11y",
+    // "@chromatic-com/storybook"
   ],
   "framework": {
     "name": "@storybook/react-vite",
--- src/components/ui/Page.stories.ts
+++ src/components/ui/Page.stories.ts
@@ -8,6 +8,9 @@ const meta = {
   parameters: {
     // More on how to position stories at: https://storybook.js.org/docs/configure/story-layout
     layout: 'fullscreen',
+    a11y: {
+      test: 'off',
+    },
   },
 } satisfies Meta<typeof Page>;
 
