7df1351eceefa6c624b299bbb06251494568f509
Yann Braga <yannbf@gmail.com>
2025-04-23 16:38:40 +0200

    remove unused glob

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,20 +1,17 @@
-import type { StorybookConfig } from '@storybook/react-vite';
+import type { StorybookConfig } from '@storybook/react-vite'
 
 const config: StorybookConfig = {
-  "stories": [
-    "../src/**/*.mdx",
-    "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"
-  ],
-  "addons": [
-    "@storybook/addon-onboarding",
-    "@storybook/addon-docs",
-    "@storybook/addon-vitest",
-    "@storybook/addon-a11y"
+  stories: ['../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
+  addons: [
+    '@storybook/addon-onboarding',
+    '@storybook/addon-docs',
+    '@storybook/addon-vitest',
+    '@storybook/addon-a11y',
     // "@chromatic-com/storybook
   ],
-  "framework": {
-    "name": "@storybook/react-vite",
-    "options": {}
-  }
-};
-export default config;
+  framework: {
+    name: '@storybook/react-vite',
+    options: {},
+  },
+}
+export default config
