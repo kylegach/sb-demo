5e8749aae05ea95c00d6884a36df6ab89b913dd2
Yann Braga <yannbf@gmail.com>
2025-04-23 16:45:31 +0200

    updates

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
-import path from "path"
-import tailwindcss from "@tailwindcss/vite"
-import { defineConfig } from 'vite'
+import path from 'path'
+import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from 'vitest/config'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
@@ -8,7 +8,21 @@ export default defineConfig({
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
+  test: {
+    coverage: {
+      exclude: [
+        'src/App.tsx',
+        'src/main.tsx',
+        '.storybook',
+        'src/vite-env.d.ts',
+        'eslint.config.js',
+        'vite.config.ts',
+        'vitest.shims.d.ts',
+        'vitest.workspace.ts',
+      ],
     },
   },
 })
